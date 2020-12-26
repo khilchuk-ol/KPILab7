ffc8b754c4cd98590a14afec7cd51b0cd119d07f
Olena Khilchuk <olena.khilchuk@gmail.com>
2020-12-26 18:41:55 +0200

    mongodb

--- InfoReceive/InfoReceiver.cs
+++ InfoReceive/InfoReceiver.cs
@@ -4,6 +4,7 @@ using RabbitMQ.Client.Events;
 using System.Text;
 using Newtonsoft.Json;
 using System.Linq;
+using MongoDB.Driver;
 
 namespace InfoReceive
 {
@@ -11,6 +12,10 @@ namespace InfoReceive
     {
         static void Main()
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var db = client.GetDatabase("ErrorRabbitDB");
+            var collection = db.GetCollection<Message>("messages");
+
             var factory = new ConnectionFactory() { HostName = "localhost" };
 
             using (var connection = factory.CreateConnection())
@@ -35,11 +40,7 @@ namespace InfoReceive
                         var json = Encoding.UTF8.GetString(body);
                         var msg = JsonConvert.DeserializeObject<Message>(json);
 
-                        using(var ctx = new AppContext())
-                        {
-                            ctx.Messages.Add(msg);
-                            ctx.SaveChanges();
-                        }
+                        collection.InsertOne(msg);
 
                         Console.WriteLine($" [x] Received '{eargs.RoutingKey}':'{msg.Text}' created at '{msg.CreateTime}' and received at '{msg.ReceiveTime}'");
                     };
